c09102d681f94a93715777a06a7b91812e3dbc9d
Andrew Ganzha <jackbrikman@mail.ru>
2025-12-22 15:21:02 +0300

    Add core config

--- app/main.py
+++ app/main.py
@@ -1,7 +1,11 @@
 import uvicorn
+
+from api import router as api_router
 from fastapi import FastAPI
+from core.config import Settings, settings
 
 app = FastAPI()
+app.include_router(api_router, prefix=settings.api.prefix)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
